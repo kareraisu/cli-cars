1ca77823305c9cc5c38ae3d01acdf26821524060
Edgardo Martinez <edgardom@bluetrailsoft.com>
2022-05-02 10:51:20 -0300

    - Agregar mensaje de Bienvenida con las opciones. - Capturar la opcion ingresada por teclado. - Obtener datos del CSV.

--- cli.js
+++ cli.js
@@ -1,25 +1,79 @@
 #!/usr/bin/env node
-import meow from 'meow';
+import * as readline from 'node:readline/promises';
 
-const cli = meow(`
-	Usage
-	  $ unicorn-fun [input]
+// Obtener los datos, un CSV
+// 1 como hacer la request
+// - Que sucede si no tengo conexion?. Manejar los errores.
+// 2 Como convertir un CSV a un JSON
+// 3 Ver como filtar segun las opciones
 
-	Options
-	  --postfix  Lorem ipsum  [Default: rainbows]
+let URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRXhp9bt3GgfL0ZwpC05_DOH2eIAK4ojTTXKdg_tdl9Gg07TFZnbKI8lsNtLJ14EaI218cyu23f25LE/pub?gid=0&single=true&output=csv"
 
-	Examples
-	  $ cli-name
-	  unicorns & rainbows
-	  $ cli-name ponies
-	  ponies & rainbows
-`, {
-	flags: {
-		postfix: {
-			type: 'string',
-			default: 'rainbows'
-		}
-	}
+let response = await fetch(URL)
+
+let data = await response.text()
+
+console.log(data)
+
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout,
 });
 
-console.log((cli.input[0] || 'unicorns') + ' & ' + Object.values(cli.flags));
+
+//usage inside aync function do not need closure demo only*
+(async () => {
+	try {
+		let filterOption =
+			await rl.question(`Hola soy catalogo de auto, que filtros dease aplicar
+		1. Brand
+		2. Model
+		3. Year
+		4. Motor/engine
+		5. Type
+		6. Purpose
+`);
+		filterOption = parseInt(filterOption);
+
+		if (filterOption >= 1 && filterOption <= 6) {
+			switch (filterOption) {
+				case 1:
+					console.log("Filter by BRAND");
+					break;
+				case 2:
+					// code block
+					console.log("Filter by MODEL");
+					break;
+				case 3:
+					// code block
+					console.log("Filter by YEAR");
+					break;
+				case 4:
+					// code block
+					console.log("Filter by MOTOR/ENGINE");
+					break;
+				case 5:
+					// code block
+					console.log("Filter by TYPE");
+					break;
+				case 6:
+					// code block
+					console.log("Filter by PURPOSE");
+					break;
+				default:
+					// code block
+					console.log("VER Como tratar otras opciones");
+			}
+		} else {
+			filterOption = await rl.question(`Elegi una opcion entre 1 y 6`)
+		}
+
+		rl.close();
+	} catch (e) {
+		console.error("unable to rl.question", e);
+	}
+})();
+
+//when done reading rl.question exit program
+rl.on("close", () => process.exit(0));
